1af1f53050ac83bb08660afdf6f72ca37507026d
Simon Prickett <simon@crudworks.org>
2024-10-07 14:52:41 +0100

    Initial commit.

---
+++ README.md
@@ -0,0 +1,30 @@
+# MicroCrate - A CrateDB Driver for MicroPython
+
+## Introduction
+
+TODO
+
+## Installation
+
+### Installing Dependencies
+
+This driver uses the base64 implementation from [`micropython-lib`](https://github.com/micropython/micropython-lib/).  Install it on your device by first establishing a network connection, then entering the following commands:
+
+```python
+import mip
+mip.install('base64')
+```
+
+### Installing MicroCrate
+
+TODO
+
+## Connecting to CrateDB
+
+TODO
+
+## Interacting with CrateDB
+
+TODO
+
+For more examples, check out [`example_usage.py`](./example_usage.py).
---
+++ example_usage.py
@@ -0,0 +1,87 @@
+import microcrate
+
+# CrateDB Docker / local network, no SSL.
+# crate = microcrate.CrateDB(host="192.168.5.4", use_ssl=False)
+
+# CrateDB Cloud.
+crate = microcrate.CrateDB(
+    host="some-example-cluster.eks1.eu-west-1.aws.cratedb.net", 
+    user="someuser", 
+    password="secretSqu1rre199*!"
+)
+
+try:
+    # Create a table.
+    print("Create table.")
+    response = crate.execute("create table driver_test(id TEXT, val1 bigint, val2 bigint, val3 boolean)")
+    # response:
+    # {'rows': [[]], 'rowcount': 1, 'cols': [], 'duration': 119.652275}
+    print(response)
+
+    # Bulk INSERT.
+    print("Bulk insert.")
+    response = crate.execute(
+        "insert into driver_test (id, val1, val2, val3) values (?, ?, ?, ?)",
+        [
+            [ "a", 2, 3, True ],
+            [ "b", 3, 4, False ]
+        ]
+    )
+    # response:
+    # {'results': [{'rowcount': 1}, {'rowcount': 1}], 'cols': [], 'duration': 6.265751}
+    print(response)
+
+    # Basic SELECT, also returning column data types.
+    print("Select with column data types.")
+    response = crate.execute("select * from driver_test", with_types=True)
+    # response:
+    # {'col_types': [4, 10, 10, 3], 'cols': ['id', 'val1', 'val2', 'val3'], 'rowcount': 2, 'rows': [['b', 3, 4, False], ['a', 2, 3, True]], 'duration': 4.378391}
+    print(response)
+
+    # SELECT with parameter substitution.
+    print("Select with parameter substitution.")
+    response = crate.execute(
+        "select val1, val2 from driver_test where val1 > ? and val2 < ?",
+        [ 1, 4 ]
+    )
+    # response:
+    # {'rows': [[2, 3]], 'rowcount': 1, 'cols': ['val1', 'val2'], 'duration': 3.266117}
+    print(response)
+
+    # INSERT with parameter substitution.
+    print("Insert with parameter substitution.")
+    response = crate.execute(
+        "insert into driver_test (id, val1, val2, val3) values (?, ?, ?, ?)",
+        [ "d", 1, 9, False ]
+    )
+    # response:
+    # {'rows': [[]], 'rowcount': 1, 'cols': [], 'duration': 5.195949}
+    print(response)
+
+    # INSERT with parameter substitution, no parsing or return of response document.
+    print("Insert with parameter substitution and no response processing.")
+    response = crate.execute(
+        "insert into driver_test (id, val1, val2, val3) values (?, ?, ?, ?)",
+        [ "e", 4, 12, True ],
+        return_response=False
+    )
+    # response:
+    # None
+    print(response)
+
+    # Drop the table.
+    print("Drop table...")
+    response = crate.execute("drop table driver_test")
+    # response:
+    # {'rows': [[]], 'rowcount': 1, 'cols': [], 'duration': 32.84445}
+    print(response)
+
+    # This will throw a CrateDBError as we dropped the table.
+    response = crate.execute("select * from driver_test")
+
+except microcrate.NetworkError as e:
+    print("Caught NetworkError:")
+    print(e)
+except microcrate.CrateDBError as c:
+    print("Caught CrateDBError:")
+    print(c)
